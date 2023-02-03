b4297e77530962872d6c50ba551da2d4d0884026
fpigeonjr <frank.pigeonjr@gmail.com>
2023-02-02 20:36:13 -0600

    makes progress on the progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants"
+import VisuallyHidden from "../VisuallyHidden"
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
-};
+  return (
+    <Wrapper>
+      <progress id="loadinglabel" max="100" value={value}>
+        {value}
+      </progress>
+    </Wrapper>
+  )
+}
 
-export default ProgressBar;
+const Wrapper = styled.div`
+  // box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+
+export default ProgressBar
